fd7b8565dc2dfcc5928db6e11fd226185fdc1ee7
youngkxxn <youngkxxn@naver.com>
2022-09-08 09:32:43 +0900

    complete

--- test2.js
+++ test2.js
@@ -1,27 +1,48 @@
 
 
 let img;
+let bx;
+let by;
+let imgSize = 100;
+let overImg = false;
+let clicked = false;
 
 function preload() {
 	img = loadAni('./tst001.png', 7);
 }
 
 function setup() {
-	createCanvas(800, 300);
+	createCanvas(400, 400);
+	bx = width / 2;
+	by = height / 2;
 }
 
 function draw() {
-	background(255);
+	background(150);
+
+	// 그림 위에 커서가 있는지 확인
+	if (
+		mouseX > bx - imgSize &&
+		mouseX < bx + imgSize &&
+		mouseY > by - imgSize &&
+		mouseY < by + imgSize
+	) {
+		overImg = true;
+	} else {
+		overImg = false;
+	}
 
 	// makes a loop between the last few frames of the animation
-	// goes from 7 to 11 and then back to 7
+	// goes from 6 to 7 and then back to 5
 	if (img.frame == img.lastFrame) {
 		img.frame = 5;
 	}
 
-	animation(img, 300, 150);
+	animation(img, 200, 200);
 }
 
 function mousePressed() {
-	img.frame = 0;
+	if(overImg) {
+		img.frame = 0;
+	}
 }
