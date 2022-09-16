6f65c0bc3526a667311f3a8de8cae78f1179f5b7
youngkxxn <youngkxxn@naver.com>
2022-09-16 12:15:26 +0900

    Complete

--- anitst.js
+++ anitst.js
@@ -5,7 +5,7 @@ let x;
 let y; //x와 y좌표 선언
 let i = 0;
 let j = 0;
-let r; //랜덤값
+let r; //랜덤값(0~3)
 let imgHeight = 100;
 let imgWidth = 100; //이미지 사이즈는 250x250인데, 기린 클릭할때만 효과 먹히도록 조정
 let overImg = false; //마우스가 그림위에 있는지
@@ -13,8 +13,15 @@ let clicked = false; //클릭했는지
 
 function preload() {
 	//절대경로는 html기준
-	Set1 = loadAni('./images/set1_001.png', 7);
-	Set2 = loadAni('./images/set2_001.png', 4);
+	Set1 = loadAni('./set1_001.png', 7);
+	Set2 = loadAni('./set2_001.png', 4);
+
+	// 0~9 난수생성
+	r = Math.floor(Math.random() * 10);
+
+	// 1~3 사이 난수로 가공 (확률은 1이 약간 더 많이 나옴)
+	r = r % 3 + 1
+
 }
 
 function setup() {
@@ -45,23 +52,23 @@ function draw() {
 		overImg = false;
 	}
 
-	//0~9 난수생성
-	// r = Math.floor(Math.random() * 10);
-
-
   ///// 여기서 멈춰있음. Set1사이사이에 Set2가 들어오게 하고 싶은데 잘 안됨.
 
-	if (Set1.frame == Set1.lastFrame && j < 2) {
+	//랜덤하게 Set1이 r1(1~3)번 돌고, 이후에 set2가 1번돌고, i=0으로 초기화
+	if (Set1.frame == Set1.lastFrame && j < r) {
 		i += 1;
 		j = Math.floor(i / 15);
-	} else if (Set2.frame == Set2.lastFrame) {
+	} else if (Set2.frame == Set2.lastFrame && j == r) {
 		i += 1;
 		j = Math.floor(i / 15);
+	} else if (j > r){
+		i = 0;
+		j = 0;
 	}
 
-	if (j < 2) {
+	if (j < r) {
 		img = Set1;
-	} else if (j >= 2) {
+	} else if (j == r) {
 		img = Set2;
 	}
 
@@ -69,7 +76,7 @@ function draw() {
 	animation(img, x, y);
 	Set1.frameDelay = 15;
 	Set2.frameDelay = 15;
-	console.log(r);
+	console.log("r=", r, "i=", i, "j=", j);
 }
 
 function mousePressed() {
