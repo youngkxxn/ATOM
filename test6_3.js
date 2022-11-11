af5b375f04c4f9f688dfc02b11a9fcbc69faa222
youngkxxn <youngkxxn@naver.com>
2022-11-11 17:29:43 +0900

    6_3 Complete

--- test6_3.js
+++ test6_3.js
@@ -19,18 +19,27 @@ function setup() {
 function draw() {
   background('#f6f5f0');
 
-
   for (var i = 0; i < s.length + 1; i++){
     if(s[i]) {
       if (s[i].overlapping(area)) {
         s[i].ani = 'normal';
+        s[i].rotation = 0;
+        s[i].rotationDrag = 100;
+        s[i].mass = 2;
       } else {
         s[i].ani = 'falling';
+        if (s[i].x < width * 0.5) {
+          s[i].rotationSpeed = -5 ;
+        } else {
+          s[i].rotationSpeed = 5 ;
+        }
+        s[i].rotationDrag = 10;
+        s[i].mass = 3;
       }
     }
   }
 
-  console.log(i)
+  console.log(s[i])
   allSprites.debug = mouse.pressing();
 }
 
@@ -38,6 +47,5 @@ function mousePressed() {
   let _t = new Sprite(mouse.x, 0, 20);
   _t.addAni('normal', './set1_001.png', 7);
   _t.addAni('falling', './set2_001.png', 4);
-  _t.rotationDrag = 10;
   s.push(_t);
 }
