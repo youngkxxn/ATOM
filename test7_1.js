7c7b2122dbe4a1a5e3c210713120cc0f3f5f2b76
youngkxxn <youngkxxn@naver.com>
2022-11-28 13:56:34 +0900

    test7-1 complete

--- test7_1.js
+++ test7_1.js
@@ -1,8 +1,6 @@
 // Class Sprite 사용법
 
 let tst;
-let i = 0;
-let a = [];
 let click = false;
 let cyc1 = false;
 let cyc2 = false;
@@ -10,40 +8,42 @@ let cyc3 = false;
 
 function preload() {
   tst = new Sprite(250, 200, 200, 200, 'static');
+  tst.addAni('bridge', './brd_001.png', 2);
   tst.addAni('set2', './set2_001.png', 3);
-  tst.ani.frameDelay = 40;
+  // tst.ani.frameDelay = 40;
   tst.addAni('set3', './set3_001.png', 4);
-  tst.ani.frameDelay = 20;
+  // tst.ani.frameDelay = 20;
   tst.addAni('set1', './set1_001.png', 5);
-  tst.ani.frameDelay = 20;
+  // tst.ani.frameDelay = 20;
 }
 
 function setup() {
   createCanvas(500, 400);
-
+  frameRate(30);
 }
 
 function draw() {
 	background('#f6f5f0');
 
-  i += 1;
-  text(frameCount, -10, -10);
-
   if(tst.ani.name == 'set1' && click && cyc1) {
-    tst.ani = 'set2';
+    tst.ani = ['set2', 'bridge'];
     tst.ani.play(0)
     cyc1 = false;
-  } else if(tst.ani.name == 'set2' && click && cyc2) {
-    tst.ani = 'set3';
+  } else if (tst.ani.name == 'bridge' && click && cyc1) {
+    tst.ani = ['set2', 'bridge'];
+    tst.ani.play(0)
+    cyc1 = false;
+  }else if(tst.ani.name == 'bridge' && click && cyc2) {
+    tst.ani = ['set3', 'bridge'];
     tst.ani.play(0)
     cyc2 = false;
-  } else if(tst.ani.name == 'set3' && click && cyc3) {
-    tst.ani = 'set1';
+  } else if(tst.ani.name == 'bridge' && click && cyc3) {
+    tst.ani = ['set1', 'bridge'];
     tst.ani.play(0)
     cyc3 = false;
   }
 
-  //조건
+  //조건 : lastFrame이 지나야 cycle이 돌았다고 판단.
   if (tst.ani.name == 'set1') {
     if(tst.ani.frame == tst.ani.lastFrame) {
       cyc1 = true;
@@ -60,7 +60,7 @@ function draw() {
 
   // allSprites.debug = mouse.pressing();
   // console.log(tst.ani.name, 'c1=', cyc1, 'c2=', cyc2, 'c3=', cyc3)
-  console.log(attackTime)
+  console.log(tst.ani.name)
 }
 
 function mousePressed() {
